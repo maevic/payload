f47a9530f19b259a2a29ac3f1a53a78cb8f33cc6
Maevic <maevic@users.noreply.github.com>
2024-01-24 17:16:33 +0100

    test(plugin-nested-docs):  add tests

--- test/_community/e2e.spec.ts
+++ test/_community/e2e.spec.ts
@@ -21,10 +21,20 @@ describe('Admin Panel', () => {
     initPageConsoleErrorCatch(page)
   })
 
-  test('example test', async () => {
-    await page.goto(url.list)
+  test('version exists', async () => {
+    await page.goto(url.edit('1'))
+    const versionsTab = page.locator('.doc-tabs__tabs li:nth-child(2)')
+    await versionsTab.click()
+    await expect(page.locator('.row-1')).toBeAttached()
+  })
 
-    const textCell = page.locator('.row-1 .cell-text')
-    await expect(textCell).toHaveText('example post')
+  test('version exists after save', async () => {
+    await page.goto(url.edit('1'))
+    const saveButton = page.locator('#action-save')
+    await saveButton.click()
+    const verionsTab = page.locator('.doc-tabs__tabs li:nth-child(2)')
+    await verionsTab.click()
+    await expect(page.locator('.row-1')).toBeAttached()
+    await expect(page.locator('.row-2')).toBeAttached()
   })
 })
